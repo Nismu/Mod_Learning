f9c10322a4304d8af4d4ed35c62d2ade02960bcd
Nismu <jpagan.ruben@gmail.com>
2014-06-26 21:18:36 -0400

    adding code

---
+++ src/main/java/com/Nismu/Mod_Learning/Mod_Learning.java
@@ -0,0 +1,32 @@
+package com.Nismu.Mod_Learning;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+
+@Mod(modid = "Mod_Learning", name = "Mod Learning", version = "0.01")
+public class Mod_Learning
+{
+    @Mod.Instance("Mod_Learning")
+    public static Mod_Learning instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
